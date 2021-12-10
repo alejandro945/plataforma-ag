f8631a6451673601e38c1d869de165e3cc2613a1
alejandro945 <alejo8677@gmail.com>
2021-12-10 17:18:50 -0500

    :boom: Crud Operations in User Entity

--- server/src/services/users/controller.js
+++ server/src/services/users/controller.js
@@ -1,38 +1,50 @@
-const userProvider = require('./provider')
 const db = require('../../db/models')
+const jwt = require('jsonwebtoken');
+const key = require('../../config/auth.config');
+const bcrypt = require('bcryptjs');
+const { Op } = require("sequelize");
 
 function authenticate(req, res) {
-    userProvider.authenticate(req.body, result => {
-        console.log(result)
-        if (result.state) {
-            res.json(result).status(200);
-        } else {
-            res.sendStatus(500);
+    db.User.findOne({
+        where: {
+            usuario: req.body.username
         }
     })
-}
-
-function getUsers(_, res) {
-    return db.User
-        .findAll({ attributes: {exclude: ['contraseña']}})
-        .then(results => res.status(200).json(results))
-        .catch(error => res.status(400).send(error))
+        .then(results => {
+            if (bcrypt.compareSync(req.body.password, results.contraseña)) {
+                results.changed('updatedAt', true)
+                results.save()
+                const token = jwt.sign({ sub: results.id, role: results.role }, key.secret);
+                const { contraseña, ...userWithoutPassword } = results.dataValues;
+                res.status(200).json({ userWithoutPassword, token });
+            } else {
+                res.sendStatus(400);
+            }
+        })
+        .catch(error => res.status(500).send(error))
 }
 
 function getById(req, res) {
     const { id } = req.params;
     return db.User
-    .findOne({
-        where: {
-            id: id
-        },
-        attributes: {exclude: ['contraseña']}
-    })
-    .then(results => res.status(200).send(results))
-    .catch(error => res.status(400).send(error))
+        .findOne({
+            where: {
+                id: id
+            },
+            attributes: { exclude: ['contraseña'] }
+        })
+        .then(results => res.status(200).send(results))
+        .catch(error => res.status(400).send(error))
 }
 
-function addUser(req, res) {
+function get(_, res) {
+    return db.User
+        .findAll({ attributes: { exclude: ['contraseña'] } })
+        .then(results => res.status(200).json(results))
+        .catch(error => res.status(400).send(error))
+}
+
+function add(req, res) {
     return db.User.findOrCreate({
         where: {
             usuario: req.body.username,
@@ -49,9 +61,37 @@ function addUser(req, res) {
         .catch(error => res.status(400).send(error))
 }
 
+function update(req, res) {
+    const { id } = req.params;
+    const data = {
+        usuario: req.body.username,
+        contraseña: req.body.password,
+        role: req.body.role,
+        nombres: req.body.name,
+        apellidos: req.body.lastName,
+        estado: req.body.state
+    }
+    db.User.findOne({
+        where: {
+            usuario: data.usuario
+        }
+    }).then(res.status(500)).catch(db.User.update(data, { where: { id: id } })
+        .then(results => res.status(200).json(results))
+        .catch(error => res.status(400).send(error)));
+}
+
+function destroy(req, res) {
+    const { id } = req.params;
+    db.User.destroy({ where: { id: id } })
+        .then(results => res.status(200).json(results))
+        .catch(error => res.status(400).send(error))
+}
+
 module.exports = {
-    getUsers,
     getById,
     authenticate,
-    addUser
+    get,
+    add,
+    update,
+    destroy
 }
--- server/src/services/users/routes.js
+++ server/src/services/users/routes.js
@@ -4,9 +4,11 @@ const authorize = require('../../middleware/authorize')
 const userController = require('./controller')
 const Role = require('../../helpers/role')
 
-router.get('/',authorize(Role.Admin), userController.getUsers)
+router.get('/',authorize(Role.Admin), userController.get)
 router.get('/:id',userController.getById)
 router.post('/auth',userController.authenticate)
-router.post('/register',userController.addUser)
+router.post('/new',userController.add)
+router.put('/:id',userController.update)
+router.delete('/:id',userController.destroy)
 
 module.exports = router
